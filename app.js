ddbb6002be2473fbd7cd466d659f6efa470280d7
MISSION-NB\uark <mission.tsai@u-ark.com>
2022-07-22 17:45:46 +0800

    create register endpoint

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const dbConnent = require('./db/dbConnect')
+const bcrypt = require('bcrypt')
+const User = require('./db/userModel')
+
+
 // execute database connection
 dbConnent()
 // body parser configuration
@@ -13,5 +17,33 @@ app.get("/", (request, response, next) => {
   next();
 });
 
+app.post('/register',async(req,res)=>{
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // create a new user instance and collect the data
+    const user = new User({
+      email: req.body.email,
+      password: hashedPassword
+    })
+    //save the new user
+    try{
+      const result = user.save()
+      res.status(201).send({
+        message: 'User Created Successfully',
+        result
+      })
+    }catch(error){
+      this.response.status(500).send({
+        message:'Error Creating user',
+        error
+      })
+    }
+  }catch(error){
+    res.status(500).send({
+      message: 'Password was not hashed successfully',
+      error
+    })
+  }
+})
 
 module.exports = app;
--- db/dbConnect.js
+++ db/dbConnect.js
@@ -5,7 +5,7 @@ async function dbConnent(){
   mongoose.connect(process.env.DB_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    // useCreateIndex: true
   }).then(()=>console.log('Successfully connected to MongoDB Atlas!')).catch(error=>console.error(error))
 }
 module.exports = dbConnent
